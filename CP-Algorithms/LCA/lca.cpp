3c476d3d57f3d4dd0e2ef9b7e4565d54f3d985db
Abhijit <adas172@gmail.com>
2020-08-29 23:03:00 +0530

    LCA CP-Algo & SPOJ Problem

---
+++ CP-Algorithms/LCA/lca.cpp
@@ -0,0 +1,119 @@
+// SPOJ Problem Link https://www.spoj.com/problems/LCA/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+struct LCA{
+
+    vector<int> height,euler,first,segtree;
+    vector<bool> visited;
+    int n;
+
+
+    LCA(vector<vector<int> > & adj, int root=0){
+        
+        n = adj.size();
+        height.resize(n);
+        first.resize(n);
+        euler.reserve(n*2);
+        visited.assign(n,false);
+        dfs(adj,root);
+        int m=euler.size();
+        segtree.resize(m*4);
+        build(1,0,m-1);
+    }
+
+    void dfs(vector<vector<int> > &adj,int node, int h=0){
+
+    visited[node]= true;
+    height[node]=h;
+    first[node]=euler.size();
+    euler.push_back(node);
+
+    for(auto to : adj[node]){
+        if(!visited[to]){
+            dfs(adj,to,h+1);
+            euler.push_back(node);
+        }
+    }
+    
+}
+
+
+    void build(int node, int b, int e){
+        if(b==e){
+            segtree[node]=euler[b];
+        }
+        else{
+            int mid=(b+e)/2;
+            build(node<<1,b,mid);
+            build(node<<1|1,mid+1,e);
+            int l = segtree[node << 1], r = segtree[node << 1 | 1];
+            segtree[node] = (height[l] < height[r]) ? l : r;
+        }
+    }
+
+    int query(int node, int b, int e, int L, int R) {
+        if (b > R || e < L)
+            return -1;
+        if (b >= L && e <= R)
+            return segtree[node];
+        int mid = (b + e) >> 1;
+
+        int left = query(node << 1, b, mid, L, R);
+        int right = query(node << 1 | 1, mid + 1, e, L, R);
+        if (left == -1) return right;
+        if (right == -1) return left;
+        return height[left] < height[right] ? left : right;
+    }
+
+    int lca(int u, int v) {
+        int left = first[u], right = first[v];
+        if (left > right)
+            swap(left, right);
+        return query(1, 0, euler.size() - 1, left, right);
+    }
+};
+
+int main()
+{
+    
+    fast_cin;
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int T;
+    cin>>T;
+    for(int t=1;t<=T;t++){
+    int N,Q,M;
+    cin>> N;
+    vector<vector<int> > adj(N+1);
+
+    for(int i=1;i<=N;i++){
+        cin>>M;
+        int u;
+        for(int j=0;j<M;j++){
+            cin>>u;
+            adj[i].push_back(u);
+        }
+    }
+    LCA l(adj,1);
+    cin>>Q;
+    cout<<"Case "<<t<<":"<<"\n";
+    while(Q--){
+        int U,V;
+        cin>>U>>V;
+       // --r,--U,--V;
+        cout<<l.lca(U,V)<<"\n";
+    }
+    }
+
+    
+    return 0;
+
+}
